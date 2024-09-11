5ecf726acfaf548d05a1d38eed219883e50ef228
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-09-11 09:11:59 +0530

    Employee Attendance

---
+++ EmpAttendance.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class EmpWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+        // UC-1: Employee Attendance
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
